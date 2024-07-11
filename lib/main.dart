2511d40b88b72a3785cab1c5314c0237960e6a2b
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-07-11 12:09:26 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       title: 'MarcaSoM',
       theme: ThemeData(
         primaryColor: Color(0xFF9B59B6),
