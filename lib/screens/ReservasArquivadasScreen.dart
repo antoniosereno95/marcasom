4858ffb3c584e64ac8debc609fcd48f22379b431
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-07-16 18:10:55 -0300

    feat: Screen de Reservas Arquivadas

---
+++ lib/screens/ReservasArquivadasScreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:marcasom/widgets/cardReservation.dart';
+
+class ReservasArquivadasScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> archivedReservations;
+
+  ReservasArquivadasScreen({required this.archivedReservations});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reservas Arquivadas'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: archivedReservations.length,
+          itemBuilder: (context, index) {
+            final reserva = archivedReservations[index];
+            return ReservationCard(reserva: reserva);
+          },
+        ),
+      ),
+    );
+  }
+}
