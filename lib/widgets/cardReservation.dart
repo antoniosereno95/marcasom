d319d60694ad0b9a807067dd565bca05dfa0ca71
Antonio Sereno <antoniojsereno@gmail.com>
2024-07-21 14:34:58 -0300

    ...

--- lib/widgets/cardReservation.dart
+++ lib/widgets/cardReservation.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:marcasom/screens/resumoDaReserva.dart';
 
 class ReservationCard extends StatelessWidget {
