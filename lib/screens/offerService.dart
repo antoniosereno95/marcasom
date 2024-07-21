f09ea0057e26ebe66dd311f168d8ff46d6fbaec6
Antonio Sereno <antoniojsereno@gmail.com>
2024-07-21 15:48:05 -0300

    Ofereçer Serviços - Formulario Simples
    Ofereçer Serviços - a logica de antes ano funcionava e era muito complexa, decidimos refatorar com um formulario simples para poder realizar a entrega do MVP

--- lib/screens/offerService.dart
+++ lib/screens/offerService.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import '../widgets/barraNavegacao.dart';
 
 class OfferService extends StatefulWidget {
@@ -12,205 +9,225 @@ class OfferService extends StatefulWidget {
 }
 
 class _OfferServiceState extends State<OfferService> {
-  int _currentStep = 1; 
-  String selectedCity = ''; 
+  final _formKey = GlobalKey<FormState>();
+  String artistName = '';
+  String selectedCity = '';
+  String selectedState = '';
+  String selectedCountry = '';
+  String musicGenre = '';
+  String availableDates = '';
+  String performanceFee = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Oferecer Serviço'),
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+            Navigator.pushNamed(context, '/home');
+          },
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 34.0),
-        child: _buildCurrentStep(),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'Informações do Artista',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 28,
+                  color: Color(0xFF2C3E50),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Nome do artista',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o nome do artista';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  artistName = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Gênero musical',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o gênero musical';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  musicGenre = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Cidade de origem',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira a cidade de origem';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  selectedCity = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Estado',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o estado';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  selectedState = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'País',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o país';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  selectedCountry = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Datas disponíveis',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira as datas disponíveis';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  availableDates = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Valor da apresentação',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o valor da apresentação';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  performanceFee = value!;
+                },
+              ),
+              SizedBox(height: 32),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: FloatingActionButton.extended(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      // Process the form data
+                    }
+                  },
+                  backgroundColor: Colors.purple,
+                  label: Icon(Icons.check, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BarraDeNavegacao(
         selectedIndex: 1,
-        onTap: (index) => _navigateToOfferService(index),
+        onTap: (index) {
+          if (index == 1) {
+            // Mantém a seleção na página atual
+          } else {
+            Navigator.pushNamed(context, '/otherPage'); // Altere '/otherPage' para a rota desejada
+          }
+        },
       ),
     );
-  }
-
-  void _navigateToOfferService(int index) {
-    if (index == 1 && _currentStep != 1) {
-      setState(() {
-        _currentStep = 1; 
-      });
-    }
-  }
-
-  Widget _buildCurrentStep() {
-    if (_currentStep == 1) {
-      return _buildCitySelectionStep();
-    } else if (_currentStep == 2) {
-      return _buildServiceDaysStep();
-    } else if (_currentStep == 3) {
-      return _buildAvailableTimeStep();
-    } else {
-      return Container();
-    }
-  }
-
-  Widget _buildCitySelectionStep() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 10),
-        Row(
-          children: [
-            SizedBox(height: 10),
-            IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              }, 
-              icon: Icon(Icons.close, color: Colors.purple),
-            ),
-          ],
-        ),
-        SizedBox(height: 20),
-        Text(
-          'Em qual cidade deseja oferecer serviço?',
-          style: TextStyle(
-            fontFamily: 'Roboto',
-            fontStyle: FontStyle.normal,
-            fontWeight: FontWeight.w500,
-            fontSize: 28,
-            height: 1,
-            color: Color(0xFF2C3E50),
-          ),
-        ),
-        SizedBox(height: 20),
-        TypeAheadField(
-          textFieldConfiguration: TextFieldConfiguration(
-            decoration: InputDecoration(
-              hintText: 'Insira o nome da cidade',
-              hintStyle: TextStyle(
-                fontFamily: 'Roboto',
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.w400,
-                fontSize: 18,
-                color: Color(0xFF2C3E50),
-              ),
-              filled: true,
-              fillColor: Colors.grey[200],
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: BorderSide.none,
-              ),
-              contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-            ),
-          ),
-          suggestionsCallback: (pattern) async {
-            return await getCities(pattern);
-          },
-          itemBuilder: (context, suggestion) {
-            return ListTile(
-              title: Text(suggestion),
-            );
-          },
-          onSuggestionSelected: (suggestion) {
-            setState(() {
-              selectedCity = suggestion;
-              _currentStep = 2; 
-            });
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _buildServiceDaysStep() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        IconButton(
-          onPressed: () {
-            setState(() {
-              _currentStep = 1; 
-            });
-          },
-          icon: Icon(Icons.arrow_back, color: Colors.purple),
-          
-        ),
-        SizedBox(height: 16),
-        Text(
-          'Dias de serviço disponíveis?',
-          style: TextStyle(
-            fontFamily: 'Roboto',
-            fontStyle: FontStyle.normal,
-            fontWeight: FontWeight.w500,
-            fontSize: 28,
-            height: 1.18,
-            color: Color(0xFF2C3E50),
-          ),
-        ),
-        SizedBox(height: 32),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: FloatingActionButton.extended(
-            onPressed: () {
-              setState(() {
-                _currentStep = 3; 
-              });
-            },
-            backgroundColor: Colors.purple,
-            label: Icon(Icons.arrow_forward, color: Colors.white),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildAvailableTimeStep() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        IconButton(
-          onPressed: () {
-            setState(() {
-              _currentStep = 2; 
-            });
-          },
-          icon: Icon(Icons.arrow_back, color: Colors.purple),
-        ),
-        SizedBox(height: 16),
-        Text(
-          'Horário disponível para tal dia',
-          style: TextStyle(
-            fontFamily: 'Roboto',
-            fontStyle: FontStyle.normal,
-            fontWeight: FontWeight.w500,
-            fontSize: 28,
-            height: 1.18,
-            color: Color(0xFF2C3E50),
-          ),
-        ),
-        SizedBox(height: 32),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: FloatingActionButton.extended(
-            onPressed: () {
-            },
-            backgroundColor: Colors.purple,
-            label: Icon(Icons.arrow_forward, color: Colors.white),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Future<List<String>> getCities(String query) async {
-    final response = await http.get(
-      Uri.parse('https://servicodados.ibge.gov.br/api/v1/localidades/municipios'),
-    );
-
-    if (response.statusCode == 200) {
-      List<dynamic> data = json.decode(response.body);
-      List<String> cities = [];
-      data.forEach((city) {
-        cities.add(city['nome'] + ', ' + city['microrregiao']['mesorregiao']['UF']['sigla']);
-      });
-      return cities.where((city) => city.toLowerCase().contains(query.toLowerCase())).toList();
-    } else {
-      throw Exception('Failed to load cities');
-    }
   }
 }
