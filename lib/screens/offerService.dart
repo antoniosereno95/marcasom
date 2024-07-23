65a8286a2ad7ded63f342dc2251086a90548d43f
Antonio Sereno <antoniojsereno@gmail.com>
2024-07-23 17:15:20 -0300

    API showDatePicker no OfferServices
    Adição da API showDatePicker na Screen de oferecer servicços

--- lib/screens/offerService.dart
+++ lib/screens/offerService.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../widgets/barraNavegacao.dart';
 
 class OfferService extends StatefulWidget {
@@ -17,6 +18,23 @@ class _OfferServiceState extends State<OfferService> {
   String musicGenre = '';
   String availableDates = '';
   String performanceFee = '';
+
+  final TextEditingController _dateController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+      locale: const Locale('pt', 'BR'),
+    );
+    if (picked != null) {
+      setState(() {
+        _dateController.text = DateFormat('dd/MM/yyyy', 'pt_BR').format(picked);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -158,8 +176,9 @@ class _OfferServiceState extends State<OfferService> {
               ),
               SizedBox(height: 20),
               TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Datas disponíveis',
+                  controller: _dateController,
+                  decoration: InputDecoration(
+                  hintText: 'Datas',
                   filled: true,
                   fillColor: Colors.grey[200],
                   border: OutlineInputBorder(
@@ -167,7 +186,9 @@ class _OfferServiceState extends State<OfferService> {
                     borderSide: BorderSide.none,
                   ),
                   contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                ),
+                ),onTap: () {
+                _selectDate(context);
+                },
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Por favor, insira as datas disponíveis';
