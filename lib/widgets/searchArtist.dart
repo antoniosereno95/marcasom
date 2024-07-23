5e0754a3c19fb06019d26b8255c152967281b7c4
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-07-23 01:00:58 -0300

    Validação dos campos e  add de comentarios

--- lib/widgets/searchArtist.dart
+++ lib/widgets/searchArtist.dart
@@ -13,6 +13,8 @@ class SearchArtist extends StatefulWidget {
 class _SearchArtistState extends State<SearchArtist> {
   final TextEditingController _cityController = TextEditingController();
   final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _genreController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>(); //Implemetei a validação dos inputs tbm
 
   Future<List<String>> _getCities(String pattern) async {
     final response = await http.get(
@@ -26,7 +28,7 @@ class _SearchArtistState extends State<SearchArtist> {
     } else {
       throw Exception('Failed to load cities');
     }
-  }
+  } //Adicionei a API dos municipios brasileiros pra dar um Plus ^-^
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
@@ -40,7 +42,7 @@ class _SearchArtistState extends State<SearchArtist> {
       setState(() {
         _dateController.text = DateFormat('dd/MM/yyyy', 'pt_BR').format(picked);
       });
-    }
+    } //Achei essa API para selecionar a data e consegui colocar em português
   }
 
   @override
@@ -52,92 +54,116 @@ class _SearchArtistState extends State<SearchArtist> {
         color: Colors.white,
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Column(
-        children: [
-          const SizedBox(height: 5),
-          TypeAheadFormField(
-            textFieldConfiguration: TextFieldConfiguration(
-              controller: _cityController,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.location_on),
-                hintText: 'Cidade',
-                border: InputBorder.none,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            const SizedBox(height: 5),
+            TypeAheadFormField(
+              textFieldConfiguration: TextFieldConfiguration(
+                controller: _cityController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.location_on),
+                  hintText: 'Cidade',
+                  border: InputBorder.none,
+                ),
               ),
-            ),
-            suggestionsCallback: (pattern) async {
-              return await _getCities(pattern);
-            },
-            itemBuilder: (context, suggestion) {
-              return ListTile(
-                title: Text(suggestion),
-              );
-            },
-            onSuggestionSelected: (suggestion) {
-              setState(() {
-                _cityController.text = suggestion;
-              });
-            },
-          ),
-          const SizedBox(height: 5),
-          Container(
-            width: double.infinity,
-            height: 1.0,
-            color: const Color(0xFFA0A0A0),
-          ),
-          const SizedBox(height: 5),
-          TextField(
-            decoration: InputDecoration(
-              prefixIcon: const Icon(Icons.music_note),
-              hintText: 'Gênero Musical',
-              border: InputBorder.none,
-            ),
-          ),
-          const SizedBox(height: 5),
-          Container(
-            width: double.infinity,
-            height: 1.0,
-            color: const Color(0xFFA0A0A0),
-          ),
-          const SizedBox(height: 5),
-          TextField(
-            controller: _dateController,
-            readOnly: true,
-            decoration: InputDecoration(
-              prefixIcon: const Icon(Icons.calendar_today),
-              hintText: 'Data',
-              border: InputBorder.none,
-            ),
-            onTap: () {
-              _selectDate(context);
-            },
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ListagemArtista()),
+              suggestionsCallback: (pattern) async {
+                return await _getCities(pattern);
+              },
+              itemBuilder: (context, suggestion) {
+                return ListTile(
+                  title: Text(suggestion),
                 );
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF9B59B6),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(10.0),
+              onSuggestionSelected: (suggestion) {
+                setState(() {
+                  _cityController.text = suggestion;
+                });
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor, insira a cidade.';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 5),
+            Container(
+              width: double.infinity,
+              height: 1.0,
+              color: const Color(0xFFA0A0A0),
+            ),
+            const SizedBox(height: 5),
+            TextFormField(
+              controller: _genreController,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.music_note),
+                hintText: 'Gênero Musical',
+                border: InputBorder.none,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor, insira o gênero musical.';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 5),
+            Container(
+              width: double.infinity,
+              height: 1.0,
+              color: const Color(0xFFA0A0A0),
+            ),
+            const SizedBox(height: 5),
+            TextFormField(
+              controller: _dateController,
+              readOnly: true,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.calendar_today),
+                hintText: 'Data',
+                border: InputBorder.none,
+              ),
+              onTap: () {
+                _selectDate(context);
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor, selecione uma data.';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ListagemArtista()),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF9B59B6),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15.0),
+                  child: Text(
+                    'Procurar',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                 ),
               ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0),
-                child: Text(
-                  'Procurar',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
