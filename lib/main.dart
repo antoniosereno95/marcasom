2bf5bdef6d00468d03e6249df8545df3d8b6413d
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-07-23 00:48:11 -0300

    Instalando e Refatorando APIs no sistema

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,14 @@ import 'screens/loadingScreen.dart';
 import 'screens/loginScreen.dart';
 import 'screens/cadastroScreen.dart';
 import 'screens/buscaInicial.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'screens/perfilScreen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(MyApp());
+  initializeDateFormatting('pt_BR', null).then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -20,24 +24,16 @@ class MyApp extends StatelessWidget {
         primaryColor: Color(0xFF9B59B6),
         secondaryHeaderColor: Color(0xFFF1C40F),
         fontFamily: 'Roboto',
-        textTheme: TextTheme(
-          displayLarge: TextStyle(fontFamily: 'Roboto'),
-          displayMedium: TextStyle(fontFamily: 'Roboto'),
-          displaySmall: TextStyle(fontFamily: 'Roboto'),
-          headlineLarge: TextStyle(fontFamily: 'Roboto'),
-          headlineMedium: TextStyle(fontFamily: 'Roboto'),
-          headlineSmall: TextStyle(fontFamily: 'Roboto'),
-          titleLarge: TextStyle(fontFamily: 'Roboto'),
-          titleMedium: TextStyle(fontFamily: 'Roboto'),
-          titleSmall: TextStyle(fontFamily: 'Roboto'),
-          bodyLarge: TextStyle(fontFamily: 'Roboto'),
-          bodyMedium: TextStyle(fontFamily: 'Roboto'),
-          bodySmall: TextStyle(fontFamily: 'Roboto'),
-          labelLarge: TextStyle(fontFamily: 'Roboto'),
-          labelMedium: TextStyle(fontFamily: 'Roboto'),
-          labelSmall: TextStyle(fontFamily: 'Roboto'),
-        ),
       ),
+            localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('pt', 'BR'),
+      ],
       home: LoadingScreen(),
       routes: {
         '/login': (context) => LoginScreen(),
